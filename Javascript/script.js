8ff693aeb654a6920ec2ad1649491dde6f08cf28
Md Mobasshir Ahmed <mobasshirahmed646@gmail.com>
2026-03-14 16:20:17 +0600

    lesson level no done dynamically

---
+++ Javascript/script.js
@@ -0,0 +1,32 @@
+// console.log("JS added")
+
+const loadLessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then ((res) => res.json())
+    .then(json => displayLesson(json.data));
+}
+
+const displayLesson = (lessons) => {
+//  console.log(lessons);
+
+// 1. get the container & empty
+const levelContainer = document.getElementById("level-container");
+levelContainer.innerHTML = "";
+// 2. get into every lessons
+for (let lesson of lessons) {
+
+    //     3. create Element
+    // console.log(lesson)
+    const btnDiv = document.createElement("div")
+    btnDiv.innerHTML = `
+        <button  class="btn btn-outline btn-primary"> <i class="fa-solid fa-book-open"></i> Lesson -${(lesson.level_no)} </button>
+    `
+//     4.appent into container
+    levelContainer.append(btnDiv)
+
+}
+//     3. create Element
+//     4.appent into container
+}
+
+loadLessons();
